117b7c93a49d48cc27339487b957c7e3a38c8fba
JuLong <julong@wemakeprice.com>
2022-05-24 16:49:49 +0900

    basic ssr

---
+++ react-ssr-18/src/server.tsx
@@ -0,0 +1,18 @@
+import express from "express";
+import React from 'react';
+import ReactDOMServer from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
+import App from "./App";
+
+let app = express();
+
+app.get("*", (req, res) => {
+  let html = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
+  res.send("<!DOCTYPE html>" + html);
+});
+
+app.listen(3004);
