9797df0931d253f4b3bfaf8fc3f1fbd4fa8e4f79
JuLong <julong@wemakeprice.com>
2022-05-27 15:42:58 +0900

    ㅁ

--- react-ssr-18-stream/src/server.tsx
+++ react-ssr-18-stream/src/server.tsx
@@ -11,42 +11,49 @@ import { atomCount, COUNT_DEFAULT_VALUE } from './store';
 
 let app = express();
 
+// 새로고침시 서버에서 axios안 날리는 이슈 있음.
+
 app.use(compression());
 app.use('/', express.static('dist', { index: ['default.html'] }));
 
 app.get("*", (req, res) => {
 
-  const initializeState = ({ set }: any) => {
-    set(atomCount, COUNT_DEFAULT_VALUE);
-  };
-
-  let html = ReactDOMServer.renderToPipeableStream(
-    <StaticRouter location={req.url}>
-      <RecoilRoot initializeState={initializeState}>
-        <Router />
-      </RecoilRoot>
-    </StaticRouter>,
-    {
-      onShellReady() {
-        // If something errored before we started streaming, we set the error code appropriately.
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
-        res.write('<!DOCTYPE html>');
-        html.pipe(res);
-        res.write('<!-- end -->');
-      },
-      onError(x) {
-        console.error(x);
-      },
-    }
-  );
   fs.readFile(path.resolve("./dist/index.html"), "utf-8", (err, data) => {
+
     if (err) {
       console.log(err);
       return res.status(500).send("Some error happened");
     }
-    
 
+    const selector = `<div id="root">`;
+    const pointIndex = data.indexOf(selector) + selector.length;
+    const start = data.slice(0, pointIndex)
+    const end = data.slice(pointIndex)
+
+    const initializeState = ({ set }: any) => {
+      set(atomCount, COUNT_DEFAULT_VALUE);
+    };
+  
+    let html = ReactDOMServer.renderToPipeableStream(
+      <StaticRouter location={req.url}>
+        <RecoilRoot initializeState={initializeState}>
+          <Router />
+        </RecoilRoot>
+      </StaticRouter>,
+      {
+        onShellReady () {
+          // If something errored before we started streaming, we set the error code appropriately.
+          res.statusCode = 200;
+          res.setHeader('Content-type', 'text/html');
+          res.write(start);
+          html.pipe(res);
+          res.write(end);
+        },
+        onError (x) {
+          console.error(x);
+        },
+      }
+    );
     // return res.send(
     //   data
     //     .replace('<div id="root"></div>', `<div id="root">${html}</div>`)
