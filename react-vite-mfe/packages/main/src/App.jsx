b5d989fe1ba18de7f2f5f16faeeb0a4327264ee5
julong <julong1988@naver.com>
2022-08-14 14:19:43 +0900

    a

--- react-vite-mfe/packages/main/src/App.jsx
+++ react-vite-mfe/packages/main/src/App.jsx
@@ -1,11 +1,20 @@
+import React, { Suspense } from 'react';
 import Button from 'remote_app/Button';
 import Button1 from 'remote_app/Button1';
 
 const App = () => {
+  const Btn1Page = React.lazy(() => import('remote_app/Button'));
+  const Btn2Page = React.lazy(() => import('remote_app/Button1'));
   return (
     <>
       <Button text="hello" />
       <Button1 text="world" />
+      <div>
+        <Suspense fallback={<div>loading...</div>}>
+          <Btn1Page text="hello suspense" />
+          <Btn2Page text="world suspense" />
+        </Suspense>
+      </div>
     </>
   )
 }
