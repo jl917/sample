4ce3c3b932bd81cd0c40b8e6e90a69de696eeb68
julong <julong1988@naver.com>
2023-01-02 21:54:00 +0900

    a

--- react-webpack-use-fetch/src/App.tsx
+++ react-webpack-use-fetch/src/App.tsx
@@ -1,4 +1,42 @@
+const wrapPromise = (promise: Promise<any>) => {
+  let status = 'pending';
+  let result: any;
+  const suspender = promise.then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    },
+  )
+  return {
+    get: () => {
+      switch (status) {
+        case 'success':
+          return result;
+        case 'error':
+          throw result;
+        default:
+          throw suspender;
+      }
+    }
+  }
+}
+
+const useFetch = (url: string) => {
+  const fetchData = fetch(url);
+  return {
+    todos: wrapPromise(fetchData),
+  };
+}
+
+const todos = useFetch('https://jsonplaceholder.typicode.com/todos')
+
 const App = () => {
+  const r = todos.todos.get();
+  console.log(r.json());
   return (
     <div>123</div>
   )
