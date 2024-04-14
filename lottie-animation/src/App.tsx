a8ef488404e3f76fe22cbfbc04adda0864a4c5c6
jl917 <julong1988@naver.com>
2024-04-14 15:29:27 +0900

    액션 추가

--- lottie-animation/src/App.tsx
+++ lottie-animation/src/App.tsx
@@ -3,13 +3,13 @@ import { useEffect, useRef } from "react";
 import data from './data.json';
 
 const App = () => {
-  const lottieRef = useRef<HTMLDivElement>(null);
+  const lottieRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     if (lottieRef.current) {
       lottie.loadAnimation({
         container: lottieRef.current, // the dom element that will contain the animation
-        renderer: "svg",
+        renderer: "canvas",
         loop: true,
         autoplay: true,
         animationData: data, // the path to the animation json
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <div>
       <h1>animation</h1>
-      <div ref={lottieRef} style={{width: 100, height: 100}}></div>
+      <button ref={lottieRef} onClick={() => {console.log(123123)}} style={{width: 100, height: 100}}></button>
     </div>
   );
 };
