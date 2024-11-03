f9599b11ae000ba2315ddab16d9d9b699c674b99
jl917 <julong1988@naver.com>
2024-11-03 19:23:08 +0900

    cssinjs지원

--- design-system/src/components/Modal/Modal.tsx
+++ design-system/src/components/Modal/Modal.tsx
@@ -1,10 +1,21 @@
 // src/components/Button/Button.tsx
 import React from "react";
-
+import { styled } from "styled-components";
 export interface ModalProps {
   show?: boolean;
 }
 
 export const Modal = ({ show }: ModalProps) => {
-  return <div>hellomodal {show ? "보이기" : "숨기기ㅁ"}</div>;
+  return (
+    <div>
+      <Title>ModalTitle</Title>
+      hellomodal {show ? "보이기" : "숨기기ㅁ"}
+    </div>
+  );
 };
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
